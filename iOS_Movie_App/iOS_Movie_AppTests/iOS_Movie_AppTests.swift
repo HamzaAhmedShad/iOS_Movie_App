689b8fb5c796dde0b1bc5ea98ae15b920ca729d8
Hamza Ahmed Shad <hamza.ahmed@brainstation-23.com>
2024-10-14 11:24:57 +0600

    init Commit

---
+++ iOS_Movie_App/iOS_Movie_AppTests/iOS_Movie_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Movie_AppTests.swift
+//  iOS_Movie_AppTests
+//
+//  Created by Hamza on 14/10/24.
+//
+
+import Testing
+@testable import iOS_Movie_App
+
+struct iOS_Movie_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
