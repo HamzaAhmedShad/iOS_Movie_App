020453805977d594554de99d77d3978ee8e5659d
Hamza Ahmed Shad <35128191+HamzaAhmedShad@users.noreply.github.com>
2024-10-15 12:13:16 +0600

    init Commit

---
+++ iOS_Capstone_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Capstone_Project
+//
+//  Created by Hamza on 15/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS_Capstone_ProjectTests/iOS_Capstone_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Capstone_ProjectTests.swift
+//  iOS_Capstone_ProjectTests
+//
+//  Created by Hamza on 15/10/24.
+//
+
+import Testing
+@testable import iOS_Capstone_Project
+
+struct iOS_Capstone_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
